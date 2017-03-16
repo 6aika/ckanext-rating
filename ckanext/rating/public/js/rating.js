d0d66c0d0fe547727d5cafc931d90464f1ea4800
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-03-16 14:19:35 +0200

    Add a rating_single element to provide a single element for displaying and inputting a rating

--- ckanext/rating/public/js/rating.js
+++ ckanext/rating/public/js/rating.js
@@ -1,10 +1,10 @@
 $('.rating-star').hover(
   function(){ 
-    $(this).addClass('rating-star-hover') 
-    $(this).prevAll().addClass('rating-star-hover') 
+    $(this).addClass('rating-star-hover');
+    $(this).prevAll().addClass('rating-star-hover') ;
   },
   function(){ 
-    $(this).removeClass('rating-star-hover') 
-    $(this).prevAll().removeClass('rating-star-hover') 
+    $(this).removeClass('rating-star-hover');
+    $(this).prevAll().removeClass('rating-star-hover');
   }
 )
